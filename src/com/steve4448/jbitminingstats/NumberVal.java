98d04db978d5c8d0c99070154a55b2bc5e546ad1
Steve4448 <hohner13@hotmail.com>
2013-03-07 18:12:38 -0500

    Modified SmoothInteger to be used strictly for the colors.

--- src/com/steve4448/jbitminingstats/NumberVal.java
+++ src/com/steve4448/jbitminingstats/NumberVal.java
@@ -1,16 +1,13 @@
 package com.steve4448.jbitminingstats;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
 public class NumberVal extends TextView {
 	private double val = 0;
 	private String formatting;
-	public SmoothInteger curColorRed = new SmoothInteger(0, 6);
-	public SmoothInteger curColorGreen = new SmoothInteger(0, 6);
-	public SmoothInteger curColorBlue = new SmoothInteger(0, 6);
+	public SmoothColorChanger curColor = new SmoothColorChanger(0, 0, 0, 0, 0, 0, 6);
 	public NumberVal(Context context) {super(context);}
 	public NumberVal(Context context, AttributeSet set) {super(context, set);}
 	public NumberVal(Context context, AttributeSet set, int defStyle) {super(context, set, defStyle);}
@@ -18,26 +15,26 @@ public class NumberVal extends TextView {
 	
 	public void setValue(double val) {
 		if(this.val > val)
-			curColorRed.set(255);
+			curColor.set(255, 0, 0);
 		else if(this.val < val)
-			curColorGreen.set(255);
-		setBackgroundColor(Color.rgb(curColorRed.value(), curColorGreen.value(), curColorBlue.value()));
+			curColor.set(0, 255, 0);
+		setBackgroundColor(curColor.value());
 		this.val = val;
+		if(transition != null && transition.isAlive())
+			transition.interrupt();
 		transition = new Thread() {
 			@Override
 			public void run() {
 				try {
-				while(!(curColorRed.done() && curColorGreen.done() && curColorBlue.done())) {
+				while(!curColor.done()) {
 					MiningStatisticsActivity.handler.post(new Runnable() {
 						@Override
 						public void run() {
-							setBackgroundColor(Color.rgb(curColorRed.value(), curColorGreen.value(), curColorBlue.value()));
+							setBackgroundColor(curColor.value());
 						}
 					});
-					curColorRed.process();
-					curColorGreen.process();
-					curColorBlue.process();
-					Thread.sleep(1000/30);
+					curColor.process();
+					Thread.sleep(1000/20);
 				}
 				} catch(InterruptedException e) {}
 				catch(Exception e) {
---
+++ src/com/steve4448/jbitminingstats/SmoothColorChanger.java
@@ -0,0 +1,83 @@
+package com.steve4448.jbitminingstats;
+
+import android.graphics.Color;
+
+/**
+ * 
+ * @author Ktaeyln
+ * (Modified by Steve4448 to be used towards colors.)
+ */
+public class SmoothColorChanger {
+	private double red, green, blue, targetRed, targetGreen, targetBlue, variance;
+	private final Object lock = new Object();
+
+	public SmoothColorChanger(int red, int green, int blue, int targetRed, int targetGreen, int targetBlue, int variance) {
+		this.red = red;
+		this.green = green;
+		this.blue = blue;
+		this.targetRed = targetRed;
+		this.targetGreen = targetGreen;
+		this.targetBlue = targetBlue;
+		this.variance = variance;
+	}
+
+	public void setTo(int targetRed, int targetGreen, int targetBlue) {
+		synchronized(lock) {
+			this.targetRed = targetRed;
+			this.targetGreen = targetGreen;
+			this.targetBlue = targetBlue;
+		}
+	}
+
+	public int getTarget() {
+		synchronized(lock) {
+			return Color.rgb((int)targetRed, (int)targetGreen, (int)targetBlue);
+		}
+	}
+
+	public int value() {
+		synchronized(lock) {
+			return Color.rgb((int)red, (int)green, (int)blue);
+		}
+	}
+
+	public void process() {
+		synchronized(lock) {
+			if(targetRed > red)
+				red += (targetRed - red + (float)(variance / 10f)) / variance;
+			else
+				red += -(red - targetRed + (float)(variance / 10f)) / variance;
+			if(Math.round(red) == Math.round(targetRed))
+				red = targetRed;
+			
+			if(targetGreen > green)
+				green += (targetGreen - green + (float)(variance / 10f)) / variance;
+			else
+				green += -(green - targetGreen + (float)(variance / 10f)) / variance;
+			if(Math.round(green) == Math.round(targetGreen))
+				green = targetGreen;
+			
+			if(targetBlue > blue)
+				blue += (targetBlue - blue + (float)(variance / 10f)) / variance;
+			else
+				blue += -(blue - targetBlue + (float)(variance / 10f)) / variance;
+			if(Math.round(blue) == Math.round(targetBlue))
+				blue = targetBlue;
+		}
+	}
+
+	public boolean done() {
+		synchronized(lock) {
+			return ((int)red == (int)targetRed) && ((int)green == (int)targetGreen) && ((int)blue == (int)targetBlue);
+		}
+	}
+
+	void set(int newRed, int newGreen, int newBlue) {
+		synchronized(lock) {
+			this.red = newRed;
+			this.green = newGreen;
+			this.blue = newBlue;
+		}
+	}
+
+}
