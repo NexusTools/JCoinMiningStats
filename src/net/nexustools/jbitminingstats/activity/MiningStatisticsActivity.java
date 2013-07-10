3300d83ed1d507c0c55d3157c95c57c5c3dc9d5c
Steve4448 <hohner13@hotmail.com>
2013-07-10 19:07:25 -0400

    Previous commit fixed #2.

--- src/net/nexustools/jbitminingstats/activity/MiningStatisticsActivity.java
+++ src/net/nexustools/jbitminingstats/activity/MiningStatisticsActivity.java
@@ -123,13 +123,13 @@ public class MiningStatisticsActivity extends Activity {
 		if(dialogHelper.areAnyDialogsShowing()) {
 			workScheduler.schedule(minerBlockFetchTask = new TimerTask() {
 				@Override
-                public void run() {
-	                if(!dialogHelper.areAnyDialogsShowing()) {
-	                	if(canContinue)
-	                		beginFetch();
-	                	return;
-	                }
-                }
+				public void run() {
+					if(!dialogHelper.areAnyDialogsShowing()) {
+						if(canContinue)
+							beginFetch();
+						return;
+					}
+				}
 			}, 100, 100);
 			return;
 		}
@@ -172,11 +172,13 @@ public class MiningStatisticsActivity extends Activity {
 						public void run() {
 							if(problem != null) {
 								dialogHelper.create(R.string.problem_json, problem + "\n" + getString(R.string.problem_try_again), true, false, true, R.string.problem_json_positive, 0, R.string.problem_json_negative, new Runnable() {
+									@Override
 									public void run() {
 										Toast.makeText(context, R.string.problem_json_trying_again, Toast.LENGTH_SHORT).show();
 										canContinue = true;
 									}
 								}, null, new Runnable() {
+									@Override
 									public void run() {
 										Toast.makeText(context, R.string.problem_json_not_trying_again, Toast.LENGTH_SHORT).show();
 									}
@@ -359,6 +361,7 @@ public class MiningStatisticsActivity extends Activity {
 						public void run() {
 							if(problem != null) {
 								dialogHelper.create(R.string.problem_mtgox_error, problem + "\n" + getString(R.string.problem_continue_anyway), true, false, true, R.string.problem_mtgox_positive, 0, R.string.problem_mtgox_negative, null, null, new Runnable() {
+									@Override
 									public void run() {
 										Toast.makeText(context, R.string.problem_mtgox_now_disabled, Toast.LENGTH_SHORT).show();
 										settings.setMtGoxFetchEnabled(false);
@@ -592,11 +595,13 @@ public class MiningStatisticsActivity extends Activity {
 		if(settings.canAutoConnect()) {
 			if(settings.canCheckConnectionDelays() && settings.getConnectionDelay() < CONNECTION_DELAY_WARNING_CAP) {
 				dialogHelper.create(R.string.problem_low_connection_delay, R.string.label_connection_rate_warning, true, true, true, R.string.problem_low_connection_delay_positive, R.string.problem_low_connection_delay_neutral, R.string.problem_low_connection_delay_negative, new Runnable() {
+					@Override
 					public void run() {
 						settings.setConnectionDelay(Integer.parseInt(resources.getString(R.string.connection_delay)));
 						canContinue = true;
 					}
 				}, null, new Runnable() {
+					@Override
 					public void run() {
 						settings.setCheckConnectionDelays(false);
 					}
@@ -607,11 +612,13 @@ public class MiningStatisticsActivity extends Activity {
 		if(settings.isMtGoxFetchEnabled()) {
 			if(settings.canCheckConnectionDelays() && settings.getMtGoxFetchDelay() < CONNECTION_DELAY_WARNING_CAP) {
 				dialogHelper.create(R.string.problem_low_connection_delay, R.string.label_mtgox_connection_rate_warning, true, true, true, R.string.problem_low_connection_delay_positive, R.string.problem_low_connection_delay_neutral, R.string.problem_low_connection_delay_negative, new Runnable() {
+					@Override
 					public void run() {
 						settings.setMtGoxFetchDelay(Integer.parseInt(resources.getString(R.string.mtgox_currency_exchange_fetch_rate)));
 						canContinue = true;
 					}
 				}, null, new Runnable() {
+					@Override
 					public void run() {
 						settings.setCheckConnectionDelays(false);
 					}
